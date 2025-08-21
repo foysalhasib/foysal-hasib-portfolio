c9bff3390be5c67584a7d2960c652c9f7dedd602
Foysal Hasib <faisalhasib82@gmail.com>
2025-08-22 00:10:16 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,112 @@
+// ========================== Sticky start ==============================
+window.addEventListener("scroll", function() {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
+// ========================== Sticky end ==============================
+
+
+
+
+
+
+
+
+// ========================== blog button start ==============================
+// ----------------- blogBtnOne ----------------
+const buttonOne = document.getElementById("blogBtnOne");
+function pulseOne() {
+  buttonOne.style.animation = "pulse 1s infinite";
+}
+pulseOne();
+
+
+// ----------------- blogBtnTwo ----------------
+const buttonTwo = document.getElementById("blogBtnTwo");
+function bounce() {
+  buttonTwo.style.transform = "translateY(-13px)";
+    setTimeout(() => {
+      buttonTwo.style.transform = "translateY(0)";
+    }, 150);
+}
+setInterval(bounce, 800);
+
+// ----------------- blogBtnThree ----------------
+const buttonThree = document.getElementById("blogBtnThree");
+function pulseThree() {
+  buttonThree.style.animation = "pulse 1s infinite";
+}
+pulseThree();
+// ========================== blog button end ==============================
+
+
+
+
+
+
+
+
+
+// ========================== Testimonial Start  ============================
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 50,
+  centeredSlides: true, // ✅ correct option
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  },
+
+  // Autoplay
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  }
+});
+
+
+
+
+
+// ========================== Testimonial  end ==============================
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
